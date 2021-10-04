f1976567ae0972fcf13df7e77803ee271aff14c1
David Vargas <dvargas92495@gmail.com>
2021-10-04 17:49:04 -0400

    dont need blueprint for roamjs-components

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 import { toConfig, createPage } from "roam-client";
+import { createConfigObserver } from "roamjs-components";
 
 const CONFIG = toConfig(`second-degree`);
 createPage({ title: CONFIG });
+
+createConfigObserver({
+  title: CONFIG,
+  config: {
+    versioning: true,
+    tabs: [],
+  },
+});
